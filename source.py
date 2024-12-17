f22218839f2579e7311ab06d6306a537a603d079
Hau Le <leconghau.me@gmail.com>
2024-12-17 14:16:03 +0700

    Update source.py

--- source.py
+++ source.py
@@ -73,7 +73,8 @@ def login(username, password):
 def claim():
     print("=====B4: claim")
     print("close popup")
-    pyautogui.click(1112, 411)
+    pyautogui.press('tab')
+    pyautogui.press('space')
     time.sleep(0.5)
     print("claim")
     pyautogui.click(738, 966)
@@ -88,8 +89,12 @@ def claim():
 # MAIN FUNCTION
 username = generate_random_username()
 password = '1234d@EF'
-new_url = "https://app.nodepay.ai/register?ref=JJZqh2EuYeF3qjr"
-back_to_register(new_url)
-register(username, password)
-login(username, password)
-claim()
+# new_url = "https://app.nodepay.ai/register?ref=JJZqh2EuYeF3qjr"
+# loop = 1
+new_url = input("Nhap link: ") 
+loop = int(input("Nhap so lan lap: "))
+for i in range(loop):
+    back_to_register(new_url)
+    register(username, password)
+    login(username, password)
+    claim()
